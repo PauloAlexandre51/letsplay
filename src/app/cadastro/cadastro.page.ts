9f444ba47405ea6c7379232af1162a6670da2f67
Paulo Alexandre <paulo__alexandre.souza@hotmail.com>
2022-06-04 19:04:57 -0300

    formulario reativo

--- src/app/cadastro/cadastro.page.ts
+++ src/app/cadastro/cadastro.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { Quadra } from 'src/app/model/quadra.model';
 import { QuadrasService } from 'src/app/services/quadras.service';
@@ -16,10 +17,24 @@ export class CadastroPage implements OnInit {
   public esportes: Array<Esporte> = [];
   public selected: number;
 
-  constructor(private quadraService: QuadrasService, private rota: Router, private EsporteService: EsporteService) { }
+  cadastroQuadra: FormGroup;
+  isSubmitted = false;
+
+  constructor(private quadraService: QuadrasService, private rota: Router, private EsporteService: EsporteService, public formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.esportes = this.EsporteService.getEsporte();
+
+    this.cadastroQuadra = this.formBuilder.group({
+      nome: ['', [Validators.required, Validators.minLength(3)]],
+      logradouro: ['', [Validators.required]],
+      bairro: ['', [Validators.required]],
+      cidade: ['', [Validators.required]],
+      esporte: ['', [Validators.required]],
+      valorHora: ['', [Validators.required]],
+      telefone: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      descricao: ['', [Validators.required]]
+    })
   }
 
   public cadastrar() {
@@ -27,5 +42,19 @@ export class CadastroPage implements OnInit {
       console.log(resposta);
       this.rota.navigate(['/home-admin']);
     });
+  }
+
+  get errorControl() {
+    return this.cadastroQuadra.controls;
+  }
+
+  submitForm() {
+    this.isSubmitted = true;
+    if (!this.cadastroQuadra.valid) {
+      console.log('Por favor preencha todos os campos')
+      return false;
+    } else {
+      this.cadastrar();
+    }
   }
 }
